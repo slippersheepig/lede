2beb2cedf059fbe92a816022c97ae640dc2ebb09
slippersheepig <58287293+slippersheepig@users.noreply.github.com>
2021-04-18 16:34:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,4 @@
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 git clone https://github.com/jerrykuku/luci-app-ttnode.git package/luci-app-ttnode
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
-#sed -i '$a src-git openwrt_switch_lan_play https://github.com/htynkn/openwrt-switch-lan-play.git' feeds.conf.default
-git clone https://github.com/skylovebeauty/luci-app-switch-lan-play.git package/luci-app-switch-lan-play
-git clone https://github.com/slippersheepig/switch-lan-play.git package/switch-lan-play
+sed -i '$a src-git openwrt_switch_lan_play https://github.com/htynkn/openwrt-switch-lan-play.git' feeds.conf.default
